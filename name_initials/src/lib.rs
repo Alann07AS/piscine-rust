692d102482308309d8b60f2edf3326f3a4a404fb
Alann <SchnebelenAlann@hotmail.com>
2023-05-30 15:25:03 +0200

    name_initial

---
+++ name_initials/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn initials(names: Vec<&str>) -> Vec<String> {
+    names.iter().map(|x| {
+        let split_vec:  Vec<&str> = x.split_whitespace().collect();
+        if let (Some(first_char), Some(second_char)) = (split_vec[0].chars().next(), split_vec[1].chars().next()) {
+            format!("{}. {}.",first_char, second_char).to_string()
+        } else {
+            String::from("")
+        }
+    })
+    .collect()
+}
