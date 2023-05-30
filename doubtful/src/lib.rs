7913788667001f2339ad31b30e2086a54a37f5df
Alann <SchnebelenAlann@hotmail.com>
2023-05-30 11:41:26 +0200

    doubtfull

---
+++ doubtful/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn doubtful(s: &mut String ) {
+    s.push('?');
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
