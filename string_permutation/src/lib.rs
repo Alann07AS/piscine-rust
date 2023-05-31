b79eb4d2f674ecb8b1497c26d1e98f986119deb0
Alann <SchnebelenAlann@hotmail.com>
2023-05-31 15:42:40 +0200

    permutation

--- string_permutation/src/lib.rs
+++ string_permutation/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn is_permutation(s1: &str, s2: &str) -> bool {
+    if s1.len() != s2.len() {
+        return false;
     }
+    s1.chars().map(|v| v as u32).sum::<u32>()
+    == s2.chars().map(|v| v as u32).sum::<u32>()
 }
