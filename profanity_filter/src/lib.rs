d87ae80b1bb8c023ebd918488d002a855463988e
Alann <AlannSchnebelen@hotmail.com>
2023-06-05 14:53:16 +0200

    profanity_filter

---
+++ profanity_filter/src/lib.rs
@@ -0,0 +1,25 @@
+pub struct Message {
+    pub content: String,
+    pub user: String
+}
+
+impl Message {
+    pub fn new(content: String, user: String) -> Message {
+        Message {content, user}
+    }
+    pub fn send_ms(&self) -> Option<&str> {
+        let str = self.content.as_str(); 
+        if str == "" || str.contains("stupid")  {
+            None
+        } else {
+            Some(str)
+        }
+    }
+}
+
+pub fn check_ms(ms: &Message) -> (bool, &str) {
+    match ms.send_ms() {
+        None => (false, "ERROR: illegal"),
+        Some(s) => (true, s)
+    }
+}
