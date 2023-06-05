6d6fb5b3675a37b8a407daaea947d31fdcf9c32d
Alann <AlannSchnebelen@hotmail.com>
2023-06-05 12:30:26 +0200

    panic

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -8,7 +8,7 @@ pub enum Security {
 
 pub fn fetch_data(server: Result<String, String>, security_level: Security) -> String {
     match security_level {
-        Security::Unknown => server.unwrap() ,
+        Security::Unknown => server.unwrap(),  
         Security::High => server.expect("ERROR: program stops"),
         Security::Medium => server.unwrap_or("WARNING: check the server".to_string()),
         Security::Low => server.unwrap_or_else(|s| {
