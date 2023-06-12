cf121be7a8998a615594b035c91eccba435fa9b7
Alann <AlannSchnebelen@hotmail.com>
2023-06-12 13:46:07 +0200

    matrix_ops

--- matrix/src/lib.rs
+++ matrix/src/lib.rs
@@ -22,3 +22,50 @@ impl <T: Scalar<Item = T> + Clone>  Matrix<T> {
         matrix
 	}
 }
+
+use std::ops::{ Add, Sub };
+
+
+impl<T: Scalar<Item = T> + Clone> Add for Matrix<T> {
+    type Output = Option<Matrix<T>>;
+    fn add(self, rhs: Self) -> Self::Output {
+        if self.0.len() != rhs.0.len() ||
+        self.0.len() == 0 || rhs.0.len() == 0 ||
+        self.0.iter().any(|v| v.len() != self.0[0].len() || v.len() == 0) ||
+        rhs.0.iter().any(|v| v.len() != rhs.0[0].len() || v.len() == 0)
+        {
+            return None;
+        }
+        let mut result = Self(vec![vec![T::zero(); self.0[0].len()]; self.0.len()]);
+
+        self.0.iter().enumerate().for_each(|(i, v1)| {
+            let v2 = rhs.0[i].to_owned();
+            v1.iter().enumerate().for_each(|(i2, t)| {
+                result.0[i][i2] = t.to_owned() + v2[i2].to_owned();
+            })
+        });
+        Some(result)
+    }
+}
+
+impl<T: Scalar<Item = T> + Clone> Sub for Matrix<T> {
+    type Output = Option<Matrix<T>>;
+    fn sub(self, rhs: Self) -> Self::Output {
+        if self.0.len() != rhs.0.len() ||
+        self.0.len() == 0 || rhs.0.len() == 0 ||
+        self.0.iter().any(|v| v.len() != self.0[0].len() || v.len() == 0) ||
+        rhs.0.iter().any(|v| v.len() != rhs.0[0].len() || v.len() == 0)
+        {
+            return None;
+        }
+        let mut result = Self(vec![vec![T::zero(); self.0[0].len()]; self.0.len()]);
+
+        self.0.iter().enumerate().for_each(|(i, v1)| {
+            let v2 = rhs.0[i].to_owned();
+            v1.iter().enumerate().for_each(|(i2, t)| {
+                result.0[i][i2] = t.to_owned() - v2[i2].to_owned();
+            })
+        });
+        Some(result)
+    }
+}
---
+++ matrix_ops/src/lib.rs
@@ -0,0 +1,4 @@
+// use crate::{Matrix, Scalar};
+pub use lalgebra_scalar::Scalar;
+pub use matrix::Matrix;
+pub use std::ops::{ Add, Sub };
