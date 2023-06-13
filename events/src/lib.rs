45b77ec5176e1ad2eb448d5260de00b6d8d2d3ef
Alann <AlannSchnebelen@hotmail.com>
2023-06-13 17:32:09 +0200

    event avec modulo

--- events/src/lib.rs
+++ events/src/lib.rs
@@ -42,7 +42,7 @@ impl Event<'_> {
                 size: 50, color: ( 50, 50, 50), position: Position::Bottom, content: s.to_string() 
             },
             Self::Registration(d) => Notification {
-                size: 30, color: ( 255, 2, 22), position: Position::Top, content: format!("You have {}H:{}M:{}S left before the registration ends", d.num_hours(), d.num_minutes(), d.num_seconds()).to_string() //13H:38M:14S
+                size: 30, color: ( 255, 2, 22), position: Position::Top, content: format!("You have {}H:{}M:{}S left before the registration ends", d.num_hours(), d.num_minutes()%60, d.num_seconds()%60).to_string() //13H:38M:14S
             },
             Self::Appointment(s) => Notification {
                 size: 100, color: ( 200, 200, 3), position: Position::Center, content: s.to_string()
