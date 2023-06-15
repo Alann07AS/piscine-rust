d4c980acf923c9323ff729f5b7fe7ae7524f666a
Alann <AlannSchnebelen@hotmail.com>
2023-06-15 17:35:01 +0200

    d_t_thead

--- drop_the_thread/src/lib.rs
+++ drop_the_thread/src/lib.rs
@@ -7,19 +7,50 @@ pub struct Workers {
 }
 
 impl Workers {
-    pub fn new() -> Workers {}
-    pub fn new_worker(&self, c: String) -> (usize, Thread) {}
-    pub fn track_worker(&self) -> usize {}
-    pub fn is_dropped(&self, id: usize) -> bool {}
-    pub fn add_drop(&self, id: usize) {}
+    pub fn new() -> Workers {
+        Workers { drops: Cell::new(0), states: RefCell::new(vec![]) }
+    }
+    pub fn new_worker(&self, cmd: String) -> (usize, Thread) {
+        let mut states = self.states.borrow_mut();
+        let pid = states.len();
+        states.push(false);
+        (pid, Thread::new_thread(pid, cmd, self))
+    }
+    pub fn track_worker(&self) -> usize {
+        self.states.borrow().len()
+    }
+    pub fn is_dropped(&self, id: usize) -> bool {
+        self.states.borrow()[id]
+    }
+    pub fn add_drop(&self, id: usize) {
+        if self.states.borrow()[id] {
+            panic!("{} is already dropped", id)
+        }
+        self.drops.set(self.drops.get()+ 1);
+        self.states.borrow_mut()[id] = true;
+    }
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Thread<'a> {
-    // expected public fields
+
+    pub pid: usize,//the id of the thread.
+    pub cmd: String,//the name of the thread.
+    pub parent: &'a Workers,//a link to the structure Workers. (Tip: this should be a reference).
+
 }
 
 impl<'a> Thread<'a> {
-    pub fn new_thread(p: usize, c: String, t: &'a Workers) -> Thread {}
-    pub fn skill(self) {}
+    pub fn new_thread(pid: usize, cmd: String, parent: &'a Workers) -> Thread {
+        Thread { pid, cmd, parent }
+    }
+    pub fn skill(self) {
+        drop(self)
+    }
+}
+
+impl<'a> Drop for Thread<'a> {
+    fn drop(&mut self) {
+        self.parent.add_drop(self.pid)
+    }
 }
