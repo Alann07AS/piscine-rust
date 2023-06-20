0864e2fd3fd6532ecf9cfe56d07f4b005d98989e
Alann <AlannSchnebelen@hotmail.com>
2023-06-20 11:19:01 +0200

    min connard

--- slices_to_map/src/lib.rs
+++ slices_to_map/src/lib.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 
 pub fn slices_to_map<'a, U, T: Eq + std::hash::Hash>(t: &'a [T],u: &'a [U]) -> HashMap<&'a T, &'a  U> {
-    let max = t.len().max(u.len());
-    let keys = &t[..max];
-    let values = &u[..max];
+    let min = t.len().min(u.len());
+    let keys = &t[..min];
+    let values = &u[..min];
     keys.iter().zip(values.iter()).collect()
 }
