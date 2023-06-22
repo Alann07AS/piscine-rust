d7bafd3df9cda3b41a96d153eb81c70bcf9ab2c1
Alann <AlannSchnebelen@hotmail.com>
2023-06-22 09:33:25 +0200

    rot ppp

--- rot/src/lib.rs
+++ rot/src/lib.rs
@@ -4,7 +4,7 @@ pub fn rotate(input: &str, key: i8) -> String {
     for c in input.chars() {
         if c.is_alphabetic() {
             let base = if c.is_uppercase() { b'A' } else { b'a' };
-            let rotated = ((c as u8 - base + key as u8 + 26) % 26 + base) as char;
+            let rotated = ((c as u8 - base + (key.rem_euclid(26) as u8 + 26)) % 26 + base) as char;
             result.push(rotated);
         } else {
             result.push(c);
